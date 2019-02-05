e583a5457666c2f8fd94ede52cc36e69f0e6b19b
rmejia-tajc <rmejia.tajc@gmail.com>
2019-02-05 09:04:19 -0700

    start adding NavLinks and Routes to App.js

--- refugee/src/Components/App.js
+++ refugee/src/Components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 import './App.css';
 
 import { StoriesView } from '../views';
@@ -22,6 +23,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
+      <div className="topBar">
+        <nav>
+          <div className="nav-links">
+            <NavLink to="/about-us">
+              About Us
+            </NavLink>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/add-story">Submit Your Story</NavLink>
+          </div>
+        </nav>
+      </div>
+      
+
         <h1>App Test</h1>
         <StoriesView />
       </div>
--- refugee/src/actions/storiesActions.js
+++ refugee/src/actions/storiesActions.js
@@ -7,7 +7,7 @@ export const FETCH_STORIES_FAILURE = 'FETCH_STORIES_FAILURE';
 export const getStories = () => dispatch => {
     dispatch({ type: FETCH_STORIES_START });
     axios
-        .get('http://localhost:3300/api/stories')
+        .get('https://refugeestories.herokuapp.com/api/stories')
         .then(res => {
             dispatch({ type: FETCH_STORIES_SUCCESS, payload: res.data });
         })
